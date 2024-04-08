fa2e2e9476454d0bdf17f1701a8de74c4b94c908
1skoa <142159968+1skoa@users.noreply.github.com>
2024-04-08 09:41:39 +0500

    fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('/css/auth.css') }}">
-    <script src="{{ asset('/js/auth.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('public/css/auth.css') }}">
+    <script src="{{ asset('public/js/auth.js') }}"></script>
 </head>
 <body>
 <div class="container">
